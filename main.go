e59e1699d711f287af5e5e928300b675bcd8b96c
Prakhar256 <prakharjain256@gmail.com>
2024-09-28 11:06:19 +0530

    Hello world

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+// idhar class nhi hoti package hota hai
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World ")
+	fmt.Print("Kya hua")
+	// kuch bhi auto import ke liye proxy or ethernet off karo
+}
